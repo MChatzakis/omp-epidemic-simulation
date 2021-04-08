1d13680e4da9730110db222804c41a4e77a3dd4a
Manos Chatzakis <chatzakis@ics.forth.gr>
2021-04-08 16:20:59 +0300

    upds

---
+++ source/Epidemic.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+#include "Graph.h"
+
+#define LINE_SIZE 100
+
+int TRANSMISSION = 50; /* Transmission rate is 50% */
+int MORTALITY = 34;    /* Mortality rate is 34% */
+int DURATION = 10;
+int DAYS = 200;
+
+Graph *g;
+
+void readFile(char *filename);
+void readSeedFile(char *filename);
+void writeCSVFIle(char *filename);
+void epidemic();
+
+int isGoingToTransmit()
+{
+    int min = 0, max = 100, num;
+
+    srand(time(0));
+    num = (rand() % (max - min + 1)) + min;
+    printf("isGoingToTransmit: Generated: %d\n", num);
+
+    return num < TRANSMISSION;
+}
+
+int isGoingToDie(int day)
+{
+    int min = 0, max = 100, num;
+
+    srand(time(0));
+    num = (rand() % (max - min + 1)) + min;
+    printf("isGoingToDIe: Generated: %d\n", num);
+
+    return num < (1 - pow(1 - MORTALITY, day));
+}
+
+int main()
+{
+
+    //printf("Tr: %d\n", isGoingToTransmit());
+
+    readFile("../datasets/test1");
+
+    Graph_print(g);
+
+    return 0;
+}
+
+void readFile(char *filename)
+{
+    FILE *stream;
+    char line[LINE_SIZE], *tok;
+    long from, to, size = 0, edges = 0, counter = 0;
+
+    if (!(stream = fopen(filename, "r+")))
+    {
+        perror("Could not open the file");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((fgets(line, LINE_SIZE, stream)) != NULL)
+    {
+        if (line[0] == '#')
+        {
+            continue;
+        }
+        edges++;
+    }
+
+    printf("Total edges: %ld\n", edges);
+
+    rewind(stream);
+
+    g = Graph_init(2 * edges);
+
+    while ((fgets(line, LINE_SIZE, stream)) != NULL)
+    {
+        if (line[0] == '#')
+        {
+            continue;
+        }
+
+        tok = strtok(line, "\r \t\v");
+        from = atol(tok);
+
+        tok = strtok(NULL, "\r \t\v");
+        to = atol(tok);
+
+        Graph_addConnection(g, from, to);
+    }
+
+    Graph_freeUnused(g);
+
+    printf("Total nodes: %ld\n", g->currSize);
+
+    fclose(stream);
+}
+
+void readSeedFile(char *filename)
+{
+    
+}
+
+void writeCSVFIle(char *filename)
+{
+}
--- source/Graph.h
+++ source/Graph.h
@@ -2,33 +2,240 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define BUCKETS
+#include "omp.h"
 
 typedef struct Connection
 {
-    long id;
+    long indexTo; //index of the node!
     struct Connection *next;
 } Connection;
 
 typedef struct Node
 {
     long id;
+    short isDead;
+    short isContaminated;
+    long connections;
+    Connection *connectionsHead;
 } Node;
 
 typedef struct Graph
 {
     long maxSize;
     long currSize;
-    Node **nodes;
+    Node *nodes;
 } Graph;
 
-Graph *Graph_init(long initSize)
+long Graph_binarySearch(Node *nodes, long start, long size, long id)
+{
+    int mid;
+
+    if (size >= 1)
+    {
+        mid = start + (size - 1) / 2;
+
+        if (nodes[mid].id == id)
+        {
+            return mid;
+        }
+
+        if (nodes[mid].id > id)
+        {
+            return Graph_binarySearch(nodes, start, mid - 1, id);
+        }
+
+        return Graph_binarySearch(nodes, mid + 1, size, id);
+    }
+
+    return -1;
+}
+
+Graph *Graph_init(unsigned int size)
 {
     Graph *g;
-    g = (Graph *)malloc(sizeof(Graph));
-    if(!g){
 
+    g = (Graph *)malloc(sizeof(Graph));
+    if (!g)
+    {
+        perror("Could not allocate memory for graph");
+        exit(EXIT_FAILURE);
+    }
+
+    g->maxSize = size;
+    g->currSize = 0;
+    g->nodes = (Node *)malloc(size * sizeof(Node));
+    if (!g->nodes)
+    {
+        perror("Could not allocate memory for the initial nodes");
+        exit(EXIT_FAILURE);
     }
 
     return g;
+}
+
+Node Graph_initNode(Node *nodes, long index, long id)
+{
+    assert(nodes);
+
+    nodes[index].id = id;
+    nodes[index].connections = 0;
+    nodes[index].connectionsHead = NULL;
+    nodes[index].isContaminated = 0;
+    nodes[index].isDead = 0;
+
+    return nodes[index];
+}
+
+Connection *Graph_initConn(Node *nodes, long indexFrom, long indexTo)
+{
+    Connection *curr, *prev, *conn;
+    assert(nodes);
+
+    curr = nodes[indexFrom].connectionsHead;
+    prev = NULL;
+    while (curr)
+    {
+        if (curr->indexTo == indexTo)
+        {
+            return curr;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    conn = (Connection *)malloc(sizeof(Connection));
+    if (!conn)
+    {
+        perror("Could not allocate memory for new connection");
+        exit(EXIT_FAILURE);
+    }
+
+    conn->indexTo = indexTo;
+    conn->next = NULL;
+
+    if (prev == NULL)
+    {
+        nodes[indexFrom].connectionsHead = conn;
+    }
+    else
+    {
+        prev->next = conn;
+    }
+
+    nodes[indexFrom].connections++;
+    return conn;
+}
+
+Node *Graph_addConnection(Graph *g, long ID1, long ID2)
+{
+    long indexID1 = -1, indexID2 = -1, i;
+    Connection *curr, *prev, *conn;
+    Node *nodes;
+
+    assert(g);
+
+    nodes = g->nodes;
+
+    for (i = 0; i < g->currSize; i++)
+    {
+        if (nodes[i].id == ID1)
+        {
+            indexID1 = i;
+        }
+
+        if (nodes[i].id == ID2)
+        {
+            indexID2 = i;
+        }
+
+        if (indexID1 != -1 && indexID2 != -1)
+        {
+            break;
+        }
+    }
+
+    if (indexID1 == -1)
+    {
+        indexID1 = g->currSize;
+        Graph_initNode(nodes, indexID1, ID1);
+        g->currSize++;
+    }
+
+    if (indexID2 == -1)
+    {
+        if (g->currSize >= g->maxSize)
+        {
+            g->maxSize = g->maxSize * 2;
+            g->nodes = (Node *)realloc(g->nodes, g->maxSize * sizeof(Node));
+        }
+        indexID2 = g->currSize;
+        Graph_initNode(nodes, indexID2, ID2);
+        g->currSize++;
+    }
+
+    /*Directed Graph*/
+    Graph_initConn(nodes, indexID1, indexID2);
+    Graph_initConn(nodes, indexID2, indexID1);
+
+    return nodes;
+}
+
+void Graph_print(Graph *g)
+{
+    long i;
+    Node *nodes;
+    Connection *curr;
+
+    assert(g);
+    nodes = g->nodes;
+
+    for (i = 0; i < g->currSize; i++)
+    {
+        printf("#%ld [id %ld] [conns %ld]\n", i, nodes[i].id, nodes[i].connections);
+        printf("    =>[ ");
+        curr = nodes[i].connectionsHead;
+        while (curr)
+        {
+            printf("%ld ", nodes[curr->indexTo].id);
+            curr = curr->next;
+        }
+        printf("]\n");
+    }
+}
+
+Node *Graph_freeUnused(Graph *g)
+{
+    long required_size;
+    assert(g);
+
+    g->nodes = realloc(g->nodes, g->currSize * sizeof(Node));
+    g->maxSize = g->currSize;
+
+    return g->nodes;
+}
+
+void Graph_free(Graph *g)
+{
+    Node *nodes;
+    Connection *curr, *prev;
+    long i;
+
+    assert(g);
+    nodes = g->nodes;
+
+    for (i = 0; i < g->currSize; i++)
+    {
+        curr = nodes[i].connectionsHead;
+        while (curr)
+        {
+            prev = curr;
+            curr = curr->next;
+
+            free(prev);
+            prev = NULL;
+        }
+    }
+
+    free(nodes);
+    nodes = NULL;
 }
