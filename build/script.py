96d96168cb8a696bfcbb22b208845f29cdaa5c21
Manos Chatzakis <chatzakis@ics.forth.gr>
2021-04-08 20:25:14 +0300

    Update script.py

--- build/script.py
+++ build/script.py
@@ -1,12 +1,16 @@
 import subprocess
+
 datasets = ["../datasets/facebook.txt",
             "../datasets/enron.txt", "../datasets/gnutella.txt"]
 seeds = ["../datasets/fbSeed.txt",
          "../datasets/enronSeed.txt", "../datasets/gnuSeed.txt"]
-output = subprocess.getoutput(
-    "gcc ../source/Epidemic.c -lm -fopenmp -o epidemic")
 
-print(datasets[0])
-output = subprocess.getoutput("./epidemic -f "+ datasets[0] + " -s " + seeds[0])
+#output = subprocess.getoutput(
+ #   "gcc ../source/Epidemic.c -lm -fopenmp -o epidemic")
 
-print(output)
+#output = subprocess.getoutput(
+#    "./epidemic -f " + datasets[0] + " -s " + seeds[0])
+
+s = "HAHA\nLOLO"
+s = s.split('\n',0)[-1]
+print()
